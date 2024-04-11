7878495f4d4c11ff196d6e7e98af3960ba05dd39
unknown <oscar.lesmes-p@labinfo.is.escuelaing.edu.co>
2024-04-11 18:55:07 -0500

    data SQL Respuestas parcial teorico

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,16 @@
-INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (1, 'A', 'ESTO ES UNA PRUEBA');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (1, 'B', 'b. Esto debido al alcance, el cual no va hasta el despliegue en ambiente de pruebas');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (2, 'C', 'c. Esto debido a que es la que se encarga de ir a consultar a la base, extendiendo JPA Repository');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (3, 'A', 'a. Debido a que debemos realizar la planificacion del proyecto, se realizan los sprints y revisamos la retrospectiva, por ultimo se lanza a produccion');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (4, 'B', 'b. Debido a que aqui es donde se despliega en ambiente de pruebas');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (5, 'C', 'c. Debido a que es un mapeador de modelos entidad relación');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (6, 'C', 'c. Debido a que es el que se encarga de realizar el mapeo de las entidades de una manera sencilla');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (7, 'A', 'a. Debido a que es la capacidad de soportar cambios abruptos de carga');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (8, 'D', 'd. Sprint, debido a que aqui es donde se crean los entregables');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (9, 'B', 'b. Falso, debido a que si va vertical, se aumenta la capacidad, en el ejemplo de la pizza, si vamos verticalmente esta pizza se vuelve la ultimate, pero solo es una');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (10, 'B', 'b. Debido a que es el paso final, es cuando ya pasó las pruebas de aceptación y el proyecto es válido para ser lanzado en producción');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (11, 'D', 'd. Debido a que es el primer paso del flujo SCRUM');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (12, 'A', 'a. Debido a que se prioriza en pro del negocio y evaluando los riesgos');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (13, 'B', 'b. Debido a que DevOps es una filosofia para mejorar la velocidad de entregas de una organización');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (14, 'B', 'b. Falso, debido a que esta se encarga de aumentar la cantidad de recursos, como en el ejemplo de las pizzas, en el escalamiento horizontal tenemos una cantidad grande de pizzas pequeñas');
+INSERT INTO OSCAR_LESMES (NUMERO_PREGUNTA, LETRA, ARGUMENTO) VALUES (15, 'C', 'c. Infrastructure, Software, Platform; todas como servicio');
+
